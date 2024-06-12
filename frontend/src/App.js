776d3adeb9701376ce2ad20e9a229ac73eeb0172
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-06-12 09:46:53 +0530

    Navbar added

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import CounterClass from "./components/CounterClass";
+import CounterFunction from "./components/CounterFunction";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Hello React</h1>
+      <hr />
+      <CounterClass />
+      <br />
+      <hr />
+      <CounterFunction />
+      <hr /> */}
+      <Header />
+    </div>
+  );
+}
+
+export default App;
